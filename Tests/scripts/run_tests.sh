c31711b9567e92283177cb24a1766761a9a11a25
hod-alpert <haplert@paloaltonetworks.com>
2020-06-16 15:33:53 +0300

    using google credentials only during the test

--- Tests/scripts/run_tests.sh
+++ Tests/scripts/run_tests.sh
@@ -22,6 +22,12 @@ PREVIOUS_JOB_NUMBER=`cat create_instances_build_num.txt`
 python3 ./Tests/configure_and_test_integration_instances.py -u "$USERNAME" -p "$PASSWORD" -c "$CONF_PATH" -s "$SECRET_CONF_PATH" -g "$GIT_SHA1" --ami_env "$1" -n $IS_NIGHTLY --branch "$CIRCLE_BRANCH" --build-number "$PREVIOUS_JOB_NUMBER"
 code_1=$?
 
+echo 'export GOOGLE_APPLICATION_CREDENTIALS="creds.json"' >> $BASH_ENV
+source $BASH_ENV
+cat <<EOF > "$GOOGLE_APPLICATION_CREDENTIALS"
+$GCS_ARTIFACTS_KEY
+EOF
+
 if [ $code_1 -ne 1 ] ; then
   if [ -n "${NON_AMI_RUN}" ]; then
     # non AMI
@@ -32,6 +38,7 @@ if [ $code_1 -ne 1 ] ; then
   fi
 fi
 
+rm creds.json
 code_2=$?
 let "exit_code = $code_1 + $code_2"
 
